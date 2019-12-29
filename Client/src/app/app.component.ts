a22267b38511f8a2f39f69b1e93cc9ecaa3548ec
Dave Skender <dave.skender@gmail.com>
2019-12-29 18:52:40 -0500

    add github link

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Chart, ChartPoint, ChartDataSets } from 'chart.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
