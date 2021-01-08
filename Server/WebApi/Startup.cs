3524b7914c89fb8d8fc3af47d4e3aa9683ee5948
Dave Skender <dave.skender@gmail.com>
2021-01-08 16:47:35 -0500

    more cleanup

--- Server/WebApi/Startup.cs
+++ Server/WebApi/Startup.cs
@@ -15,7 +15,8 @@ namespace WebApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime.
+        // Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -34,7 +35,8 @@ namespace WebApi
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime.
+        // Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
