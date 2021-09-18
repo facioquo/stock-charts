97320eb18743012418cbc43863dbd9b3da8a983b
Dave Skender <dave.skender@gmail.com>
2021-09-18 14:41:56 -0400

    this should work, but does not

--- Client/src/app/chart/chart.service.ts
+++ Client/src/app/chart/chart.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
-import { ChartConfiguration, ScaleOptions } from 'chart.js';
+import { Chart, ChartConfiguration, ScaleOptions } from 'chart.js';
+import { CrosshairPlugin, CrosshairOptions } from 'chartjs-plugin-crosshair';
+
+Chart.register(CrosshairPlugin);
+
 @Injectable()
 export class ChartService {
 
-    baseConfig(): ChartConfiguration {
+    baseConfig() {
 
         const commonXaxes = this.commonXAxes();
+        const crosshairOptions = this.crosshairPluginOptions
 
         const config: ChartConfiguration = {
 
@@ -28,7 +33,8 @@ export class ChartService {
                         enabled: true,
                         mode: 'index',
                         intersect: false
-                    }
+                    },
+                    crosshair: crosshairOptions
                 },
                 layout: {
                     padding: {
@@ -138,5 +144,39 @@ export class ChartService {
         };
 
         return axes;
+    }
+
+    crosshairPluginOptions(): CrosshairOptions {
+
+        const crosshairOptions: CrosshairOptions = {
+            line: {
+                color: '#F66',                                      // crosshair line color
+                width: 1                                            // crosshair line width
+            },
+            sync: {
+                enabled: true,                                      // enable trace line syncing with other charts
+                group: 1,                                           // chart group (can be unique set of groups)
+                suppressTooltips: true                              // suppress tooltips when showing a synced tracer
+            },
+            zoom: {
+                enabled: false,                                     // enable zooming
+                zoomboxBackgroundColor: 'rgba(66,133,244,0.2)',     // background color of zoom box
+                zoomboxBorderColor: '#48F',                         // border color of zoom box
+                zoomButtonText: 'Reset Zoom',                       // reset zoom button text
+                zoomButtonClass: 'reset-zoom',                      // reset zoom button class
+            },
+            snap: {
+                enabled: true
+            },
+            callbacks: {
+                beforeZoom: (start, end) => {                       // called before zoom, return false to prevent zoom
+                    return true;
+                },
+                afterZoom: (start, end) => {                        // called after zoom
+                }
+            }
+        };
+
+        return crosshairOptions;
     }
 }
