e3018035dd94175eeb722caeb014f40572a91e0d
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2022-02-12 19:23:11 -0500

    use cached data with failover

---
+++ Server/WebApi/Services/Service.Storage.cs
@@ -0,0 +1,51 @@
+using System.Text;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace WebApi.Services;
+
+public class Storage
+{
+    // STARTUP
+    public static void Startup()
+    {
+        // initialize Azure services (setup storage)
+        string awjsConnection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+
+        // main blob container
+        BlobContainerClient blobContainer = new(awjsConnection, "chart-demo");
+        blobContainer.CreateIfNotExists();
+    }
+
+    // SAVE BLOB
+    public static async Task<bool> PutBlob(string blobName, string csv)
+    {
+        BlobClient blob = GetBlobReference(blobName);
+        BlobHttpHeaders httpHeader = new()
+        {
+            ContentType = "application/json"
+        };
+
+        using (MemoryStream ms = new(Encoding.UTF8.GetBytes(csv)))
+        {
+            ms.Position = 0;
+            await blob.UploadAsync(ms, httpHeader);
+        };
+        return true;
+    }
+
+    // HELPERS
+    internal static BlobClient GetBlobReference(string blobName)
+    {
+        BlobContainerClient blobContainer = GetContainerReference("chart-demo");
+        BlobClient blob = blobContainer.GetBlobClient(blobName);
+
+        return blob;
+    }
+
+    private static BlobContainerClient GetContainerReference(string containerName)
+    {
+        string awjsConnection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+        return new BlobContainerClient(awjsConnection, containerName);
+    }
+}
