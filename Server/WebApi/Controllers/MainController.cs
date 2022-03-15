58e3ac2762ca39bfefec10a2c3d41d47cdd53f04
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2022-03-14 23:25:57 -0400

    default to SPY

--- Server/WebApi/Controllers/MainController.cs
+++ Server/WebApi/Controllers/MainController.cs
@@ -19,7 +19,7 @@ public class MainController : ControllerBase
 
     [HttpGet("quotes")]
     public IActionResult GetQuotes(
-        string symbol = "QQQ",
+        string symbol = "SPY",
         string timeSpan = "DAILY")
     {
         IEnumerable<Quote> quotes = FetchQuotes.Get(symbol, timeSpan);
