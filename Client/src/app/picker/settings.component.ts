15712faa46a25c058180d7f91b5adc851ab5153c
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2023-10-12 05:26:29 -0400

    update config dialog height

--- Client/src/app/picker/settings.component.ts
+++ Client/src/app/picker/settings.component.ts
@@ -54,6 +54,7 @@ export class SettingsComponent {
     // open settings for indicator to add
     this.picker.open(PickConfigComponent, {
       minWidth: '300px',
+      maxHeight: "90vh",
       autoFocus: "dialog",
       data: listing
     });
