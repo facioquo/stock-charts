7970189ddf3d6297da17bda2a30dd5c81058572b
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2026-05-23 01:14:39 -0400

    feat: Add favicon to indy-charts VitePress documentation site

--- tests/vitepress/.vitepress/config.ts
+++ tests/vitepress/.vitepress/config.ts
@@ -4,6 +4,13 @@ export default defineConfig({
   title: "Indy Charts",
   description: "Financial charting with @facioquo/indy-charts",
   appearance: "dark",
+  head: [
+    ["link", { rel: "icon", type: "image/svg+xml", href: "/assets/icons/favicon.svg" }],
+    ["link", { rel: "icon", type: "image/x-icon", href: "/assets/icons/favicon.ico" }],
+    ["link", { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/icons/favicon-32x32.png" }],
+    ["link", { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/icons/favicon-16x16.png" }],
+    ["link", { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/icons/apple-touch-icon.png" }]
+  ],
 
   vite: {
     ssr: {
